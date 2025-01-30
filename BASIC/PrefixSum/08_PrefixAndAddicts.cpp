47637e0d54ea6d74bb3ab8479f922e4763d54d68
Kuldeeprime01 <7351320178k@gmail.com>
2025-01-30 14:24:42 +0530

    Prefix Sum ends

--- BASIC/PrefixSum/08_PrefixAndAddicts.cpp
+++ BASIC/PrefixSum/08_PrefixAndAddicts.cpp
@@ -1,42 +1,51 @@
 #include <bits/stdc++.h>
+#include <cmath>  // Include cmath for ceil function
 using namespace std;
 
-#define int long long int
+#define int long long
 #define MOD 1000000007
 #define INF 1e18
 #define fast_io ios::sync_with_stdio(0);cin.tie(0);
-int power(int b , int p , int m){if(p == 0) return 1;if(p == 1) return b;int res = power(b, p/2, m);if(p % 2 == 0) return (res * res) % m;else return (((res * res) % m) * b) % m;}
 
 void solve() {
-    int s , n;
-    cin>>s >> n;
-    vector<int> v(n);
-    vector<int> temp(n);
-    for(int i = 0 ; i < n ; i++){
-        cin>>v[i];
+    int n, k;
+    cin >> n >> k;
+    vector<int> s(k);
+    
+    for (int i = 0; i < k; i++) {
+        cin >> s[i];
     }
-    temp[0] = v[0];
-    for(int i = 1 ; i < n ; i++){
-        temp[i] = v[i] - v[i-1];
+
+    if (k == 1) {
+        cout << "YES" << endl;
+        return;
     }
-    for(int i = 0 ; i < n-1 ; i++){
-        if(temp[i+1] < temp[i]){
-            cout<<"NO"<<endl;
+
+    vector<int> a(k);
+    
+    for (int i = 1; i < k; i++) {
+        a[i] = s[i] - s[i - 1];
+    }
+
+    for (int i = 2; i < k; i++) {
+        if (a[i] < a[i - 1]) {
+            cout << "NO" << endl;
             return;
         }
     }
-    cout<<"YES"<<endl;
+
+    if (ceil((double)s[0] / (double)(n - k + 1)) > a[1]) {
+        cout << "NO" << endl;
+        return;
+    }
+
+    cout << "YES" << endl;
 }
 
-int32_t main()
-{
-    /*#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif */
+int32_t main() {
     fast_io;
-    int t = 1;
-    cin>>t;
-    while(t--) solve();
+    int t;
+    cin >> t;
+    while (t--) solve();
     return 0;
 }
